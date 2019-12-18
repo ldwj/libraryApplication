4cda0e18b36591eaa3906d28fdcf05d32e1f543c
wenjinglidan <397121373@qq.com>
2019-12-18 22:17:02 +0800

    修改

---
+++ app/src/main/java/com/example/ldwj/baseapplication/Myapplication.java
@@ -0,0 +1,19 @@
+package com.example.ldwj.baseapplication;
+
+import com.ldwj.libraryapplication.BaseApplication;
+
+/**
+ * 项目名称：BaseApplication
+ * 类描述：
+ * 创建人：ld
+ * 创建时间：2019/12/18  21:51
+ * 修改人：
+ * 修改时间：
+ * 修改备注：
+ */
+public class Myapplication extends BaseApplication {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
--- libraryApplication/src/main/java/com/ldwj/libraryapplication/BaseApplication.java
+++ libraryApplication/src/main/java/com/ldwj/libraryapplication/BaseApplication.java
@@ -1,5 +1,7 @@
-package com.tiamaes.libraryapplication;
+package com.ldwj.libraryapplication;
 
+import android.content.Context;
+import android.support.multidex.MultiDex;
 import android.support.multidex.MultiDexApplication;
 
 /**
@@ -9,13 +11,19 @@ public class BaseApplication extends MultiDexApplication {
     private static BaseApplication mInstance = null;
 
 
-
     @Override
     public void onCreate() {
         super.onCreate();
         mInstance = this;
     }
+
     public static BaseApplication getInstance() {
         return mInstance;
+    }
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        MultiDex.install(this);
     }
 }
